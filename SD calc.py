5eee2810391affff05e43ef3315924fead3ef7a1
python3-Trying-py <46845836+python3-Trying-py@users.noreply.github.com>
2021-02-18 00:12:01 -0500

    started the project
    Started to make this. It's 12:11 am. I will work on it over time. I am tired.

---
+++ SD calc.py
@@ -0,0 +1,9 @@
+import math
+while True:
+    queM=float(input("What is the probability of the event?\n"))
+    quen=float(input("What is the sample size?\n"))
+    SDs1=(1 - queM)
+    SDs2=(queM * SDs1)
+    SDs3=(SDs2 / quen)
+    SDsF=math.sqrt(SDs3)
+    print("Standard Deviation is " + str(SDsF))
