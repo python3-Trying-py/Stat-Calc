1ca63d73ef86ffd61daadd514544f7d9b38e34f2
python3-Trying-py <46845836+python3-Trying-py@users.noreply.github.com>
2021-02-20 10:55:43 -0500

    Removed while True
    removed the while True statement so it works in the main calc

--- Data set to mean calc.py
+++ Data set to mean calc.py
@@ -1,21 +1,21 @@
-while True:
-    queDS=input("Input sample data set.\n")
-    #finds commas
-    comma=(queDS.count(","))
-    if comma>=1:
-        #purges commas
-        queDS.replace(" ", "")
-        DataSet = queDS.split(",")
-    else:
-        DataSet=queDS.split(" ")
-    #puts the floats into an array
-    floats=[]
-    for i in DataSet:
-        floats.append(float(i))
-    #adds all data values together
-    DataSetSum=sum(floats)
-    #finds number of data values
-    DataSetTotal=len(floats)
-    #divides sum by number
-    MeanDS=(DataSetSum / DataSetTotal)
-    print("Mean = " + str(MeanDS))
+queDS=input("Input sample data set.\n")
+#finds commas
+comma=(queDS.count(","))
+if comma>=1:
+    #purges commas
+    queDS.replace(" ", "")
+    DataSet = queDS.split(",")
+else:
+    DataSet=queDS.split(" ")
+#puts the floats into an array
+floats=[]
+for i in DataSet:
+    floats.append(float(i))
+#adds all data values together
+DataSetSum=sum(floats)
+#finds number of data values
+DataSetTotal=len(floats)
+#divides sum by number
+MeanDS=(DataSetSum / DataSetTotal)
+print("Mean = " + str(MeanDS))
+
