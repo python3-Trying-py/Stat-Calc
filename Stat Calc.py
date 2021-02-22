d8e7a4879b398d274baeb1d08af13ff87ce48964
python3-Trying-py <46845836+python3-Trying-py@users.noreply.github.com>
2021-02-21 23:56:45 -0500

    added z input
    i just added a z input

--- Stat Calc.py
+++ Stat Calc.py
@@ -2,7 +2,7 @@ while True:
     que1=input("Welcome to Stat Calc. What function are you Calculating?\n1)Mean\n2)Standard Deviation\n3)Zscore and Percentile\n")
     queMean="1" or "Mean"
     queSD="2" or "Standard deviation" or "SD"
-    queZ="3" or "Zscore and Percentile" or "Zscore" or "Z score"
+    queZ="3" or "Zscore and Percentile" or "Zscore" or "Z score" or "z" or "Z"
     if que1==queMean:
         exec(open('Data set to mean calc.py').read())
     elif que1==queSD:
